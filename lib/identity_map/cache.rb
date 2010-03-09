20c07b3a0074de88435f8144d51ec5622f784392
PJ Davis <pj.davis@gmail.com>
2010-03-09 09:37:27 -0600

    removed log entries from cache class

--- lib/identity_map/cache.rb
+++ lib/identity_map/cache.rb
@@ -7,10 +7,8 @@ class Cache
   def put(object)
     objects = @objects[object.class] ||= {}
     if objects[object.id]
-      RAILS_DEFAULT_LOGGER.warn "Called From Cache #{objects[object.id].__id__}"
       objects[object.id]
     else
-      RAILS_DEFAULT_LOGGER.warn "Not Found In Cache #{object.__id__}"
       objects[object.id] = object
       object
     end
